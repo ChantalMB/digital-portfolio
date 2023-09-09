79ea7799815b45b50246e63a364aa47aee0fce28
ChantalMB <chantalbrousseau@rogers.com>
2023-09-09 16:32:12 -0400

    date formatting

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -33,7 +33,7 @@ const experience = [
   {
     title: "Teaching Assistant- HIST 2000A: Medieval Europe",
     location: "Carleton University",
-    date: "September 2022-May 2023",
+    date: "September 2022 - May 2023",
     description: "Facilitated in-class activities, group discussions, and debates to encourage critical thinking and enhance understanding of historical content among students. Designed and led supplementary discussion topics to complement weekly lectures, promoting deeper engagement with course material. Managed the distribution of course materials such as lectures and assignments across digital platforms. Ensured the accessibility of course materials through methods such as adding captions to video materials. Collaborated with fellow TAs and course instructor through regular meetings to share insights and pedagogical approaches ensuring consistency and alignment in course content and assessment strategies.",
     skills: ["MS Teams", "Brightspace", "MS Office Suite"],
     icon: "teaching"
@@ -57,7 +57,7 @@ const experience = [
   {
     title: "Teaching Assistant- HIST 1001A: The Making of Europe",
     location: "Carleton University",
-    date: "September 2021-May 2022",
+    date: "September 2021 - May 2022",
     description: "Graded and provided constructive feedback on written assignments and examinations, aiding students in improving their research and writing skills. Assisted students in developing research topics, refining research methodologies, and structuring essays. Managed course-related administrative tasks, such as directing students to university resources, maintaining attendance records, organizing course materials, and ensuring the efficient functioning of digital platforms (e.g., Brightspace).",
     skills: ["Brightspace", "MS Office Suite", "Zoom"],
     icon: "teaching"
@@ -73,7 +73,7 @@ const experience = [
   {
     title: "Student Partner",
     location: "Carleton University",
-    date: "September 2020-January 2021",
+    date: "September 2020 - January 2021",
     description: "Collaborating with Dr Shawn Graham in developing the Winter 2021 course HIST4916A: Digital History + Museums. Collecting, cleaning, and formatting sets of data from Ottawa GLAM (Galleries, Libraries, Archives, and Museums) institutions for students to use in creating a GLAM workbench. Building tutorials for applications such as Jupyter Notebook and RStudio to aid students in gaining the technical skills needed for the course. Compiling relevant additional resources for students to utilize beyond the course content. Contributing to the creation of the course website.",
     skills: ["Python", "Juypter Notebook", "Hugo", "Git", "R"],
     icon: "research"
@@ -81,7 +81,7 @@ const experience = [
   {
     title: "Undergraduate Research Fellow",
     location: "Carleton University",
-    date: "October 2019-April 2020",
+    date: "October 2019 - April 2020",
     description: "George Garth Graham Undergraduate Digital History Research Fellowship. Explored the history of urbanism in ancient Rome through the lense of city-building games.",
     skills: ["Javascript", "HTML", "Markdown"],
     icon: "research"
