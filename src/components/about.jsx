c1f292fc896f8451a90f2bd0b4f450389b357359
ChantalMB <chantalbrousseau@rogers.com>
2023-09-09 01:51:17 -0400

    style fix

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -22,9 +22,7 @@ function About() {
 
         <div className= "desc">
             <p className="has-line-data" data-line-start="0" data-line-end="1"><i>Hello.</i> I'm Chantal. My undergraduate studies were in history and computer science, and now I've recently completed a combined master's degree in <b>History &amp; Data Science</b> at Carleton University under the supervision of <b><u><a href="https://shawngraham.github.io/" target="_blank"/></u></b>Dr Shawn Graham. My master's research project, <a href="https://chantalmb.github.io/MRE-MitM-2023/" target="_blank">"Metadata in the Margins: Reshaping Archives as Data Through Early Modern Marginalia"</a>, centered around the creation of an application to situate the data machine learning models are built on. The functionality of my application was then demonstrated through a case study on the automated detection of marginalia found in early modern texts.</p>
-            <br/>
             <p>Unsurprisingly, with this unusual combination of education and research, my primary area of interest is <b>digital history &amp; humanities</b>, with a current focus on social history in <b>Early Modern Europe</b>, particularly in relation to the <b>history of objects and the book.</b></p>
-            <br/>
             <p className="has-line-data" data-line-start="2" data-line-end="3">I'm also interested in the more practical applications of these concepts, exploring the ways that cultural artefacts are being digitally preserved, and how this information can be made easily accessible to the public or aide in community building and reparations.</p>
         </div>
 
